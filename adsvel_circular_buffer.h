8a494553fd9c4de64a2b58071331fd2602cce16f
rivand <RivandBlack@yandex.ru>
2019-06-04 21:52:31 +0500

    v 0.0.3
    - Modified the CMakeLists.txt.
    - Added methods the CircularBuffer::empty and the CircularBuffer::size.

--- adsvel_circular_buffer.h
+++ adsvel_circular_buffer.h
@@ -1,47 +1,55 @@
 #pragma once
 
 namespace adsvel {
-    template <class T, size_t size>
-    class CircularBuffer {
-       public:
-        void push_back(T in_val);
-        T& front();
-        void pop_front();
-        T&& take_front();
+template <class T, size_t size_> class CircularBuffer {
+public:
+  inline void push_back(T in_val);
+  inline T &front();
+  inline void pop_front();
+  inline T &&take_front();
+  inline bool empty() const noexcept;
+  inline constexpr size_t size() const noexcept { return size_; };
 
-       private:
-        size_t end_cursor_{0};
-        size_t begin_cursor_{0};
-        T data_container_[size]{};
-    };
+private:
+  size_t end_cursor_{0};
+  size_t begin_cursor_{0};
+  T data_container_[size_]{};
+};
 
-    template <class T, size_t size>
-    inline void CircularBuffer<T, size>::push_back(T in_val) {
-		data_container_[end_cursor_++] = in_val;
-        if (end_cursor_ == size) {
-            end_cursor_ = 0;
-        }
-    }
+template <class T, size_t size_>
+inline void CircularBuffer<T, size_>::push_back(T in_val) {
+  data_container_[end_cursor_++] = in_val;
+  if (end_cursor_ == size_) {
+    end_cursor_ = 0;
+  }
+}
 
-    template <class T, size_t size>
-    inline T& CircularBuffer<T, size>::front() {
-        return data_container_[begin_cursor_];
-    }
+template <class T, size_t size_> inline T &CircularBuffer<T, size_>::front() {
+  return data_container_[begin_cursor_];
+}
 
-    template <class T, size_t size>
-    inline void CircularBuffer<T, size>::pop_front() {
-        if (++begin_cursor_ == size) {
-            begin_cursor_ = 0;
-        }
-    }
+template <class T, size_t size_>
+inline void CircularBuffer<T, size_>::pop_front() {
+  if (++begin_cursor_ == size_) {
+    begin_cursor_ = 0;
+  }
+}
 
-    template <class T, size_t size>
-    inline T&& CircularBuffer<T, size>::take_front() {
-        if (++begin_cursor_ != size) {
-            return std::move(data_container_[begin_cursor_ - 1]);
-        } else {
-            begin_cursor_ = 0;
-            return std::move(data_container_[size - 1]);
-        }
-    }
-}  // namespace adsvel
+template <class T, size_t size_>
+inline T &&CircularBuffer<T, size_>::take_front() {
+  if (++begin_cursor_ != size_) {
+    return std::move(data_container_[begin_cursor_ - 1]);
+  } else {
+    begin_cursor_ = 0;
+    return std::move(data_container_[size_ - 1]);
+  }
+}
+
+template <class T, size_t size_> inline bool CircularBuffer<T, size_>::empty() const noexcept {
+  if (end_cursor_ != begin_cursor_)
+    return false;
+  else
+    return true;
+};
+
+} // namespace adsvel
