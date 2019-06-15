137e14ce75b23e4d74b36dffda0bdf7a67f3dfde
rivand <RivandBlack@yandex.ru>
2019-06-15 13:38:56 +0500

    v 0.0.4
    - Added the fill test of perfomance.

--- adsvel_circular_buffer.h
+++ adsvel_circular_buffer.h
@@ -1,55 +1,67 @@
+/**
+***************************************************************************************************************************************************************
+* @file     adsvel_circular_buffer.h
+* @author   Kuznetcov.A (Rivand) 
+* @version  v 0.0.4
+* @date     6 June 2018 ã.
+* @brief    Implementation of the ñircular buffer container class.
+***************************************************************************************************************************************************************
+*/
 #pragma once
 
 namespace adsvel {
-template <class T, size_t size_> class CircularBuffer {
-public:
-  inline void push_back(T in_val);
-  inline T &front();
-  inline void pop_front();
-  inline T &&take_front();
-  inline bool empty() const noexcept;
-  inline constexpr size_t size() const noexcept { return size_; };
+    template <class T, size_t size_>
+    class CircularBuffer {
+       public:
+        inline void push_back(T in_val);
+        inline T& front();
+        inline void pop_front();
+        inline T&& take_front();
+        inline bool empty() const noexcept;
+        inline constexpr size_t size() const noexcept { return size_; };
 
-private:
-  size_t end_cursor_{0};
-  size_t begin_cursor_{0};
-  T data_container_[size_]{};
-};
+       private:
+        size_t end_cursor_{0};
+        size_t begin_cursor_{0};
+        T data_container_[size_]{};
+    };
 
-template <class T, size_t size_>
-inline void CircularBuffer<T, size_>::push_back(T in_val) {
-  data_container_[end_cursor_++] = in_val;
-  if (end_cursor_ == size_) {
-    end_cursor_ = 0;
-  }
-}
+    template <class T, size_t size_>
+    inline void CircularBuffer<T, size_>::push_back(T in_val) {
+        data_container_[end_cursor_++] = in_val;
+        if (end_cursor_ == size_) {
+            end_cursor_ = 0;
+        }
+    }
 
-template <class T, size_t size_> inline T &CircularBuffer<T, size_>::front() {
-  return data_container_[begin_cursor_];
-}
+    template <class T, size_t size_>
+    inline T& CircularBuffer<T, size_>::front() {
+        return data_container_[begin_cursor_];
+    }
 
-template <class T, size_t size_>
-inline void CircularBuffer<T, size_>::pop_front() {
-  if (++begin_cursor_ == size_) {
-    begin_cursor_ = 0;
-  }
-}
+    template <class T, size_t size_>
+    inline void CircularBuffer<T, size_>::pop_front() {
+        if (++begin_cursor_ == size_) {
+            begin_cursor_ = 0;
+        }
+    }
 
-template <class T, size_t size_>
-inline T &&CircularBuffer<T, size_>::take_front() {
-  if (++begin_cursor_ != size_) {
-    return std::move(data_container_[begin_cursor_ - 1]);
-  } else {
-    begin_cursor_ = 0;
-    return std::move(data_container_[size_ - 1]);
-  }
-}
+    template <class T, size_t size_>
+    inline T&& CircularBuffer<T, size_>::take_front() {
+        if (++begin_cursor_ != size_) {
+            return std::move(data_container_[begin_cursor_ - 1]);
+        } else {
+            begin_cursor_ = 0;
+            return std::move(data_container_[size_ - 1]);
+        }
+    }
 
-template <class T, size_t size_> inline bool CircularBuffer<T, size_>::empty() const noexcept {
-  if (end_cursor_ != begin_cursor_)
-    return false;
-  else
-    return true;
-};
+    template <class T, size_t size_>
+    inline bool CircularBuffer<T, size_>::empty() const noexcept {
+        if (end_cursor_ != begin_cursor_)
+            return false;
+        else
+            return true;
+    };
 
-} // namespace adsvel
+}  // namespace adsvel
--- cmake_test/cmake_test.cpp
+++ cmake_test/cmake_test.cpp
@@ -1,18 +1,47 @@
-﻿#include <iostream>
+﻿#include <array>
+#include <chrono>
+#include <iostream>
 #include "../adsvel_circular_buffer.h"
-using adsvel::CircularBuffer;
+#include "boost/circular_buffer.hpp"
+using std::cin;
 using std::cout;
 using std::endl;
+using std::chrono::high_resolution_clock;
+using std::chrono::time_point;
 int main() {
-    CircularBuffer<int16_t, 32> cb;
-    cout << "This is adsvel::CircularBuffer." << endl;
-    cb.push_back(1);
-    cb.push_back(2);
-    cb.push_back(3);
-    cb.push_back(4);
-    cb.push_back(5);
-    cout << cb.front() << " ";
-    for (int i = 0; i < 5; i++) cout << cb.take_front() << " ";
+    constexpr size_t kSizeOfTestSet{1000};
+    constexpr size_t kSizeOfTestBuffers{1000};
+    typedef int tTestType;
+    srand(static_cast<int>(high_resolution_clock::now().time_since_epoch().count()));
+    std::array<tTestType, kSizeOfTestSet> test_data{};
+    for (auto& c : test_data) c = rand();
+
+    uint64_t amountr_of_test_iterations{0};
+    cout << "Enter the number of iterations in the test: ";
+    cin >> amountr_of_test_iterations;
+    // fill test
+	{
+		adsvel::CircularBuffer<tTestType, kSizeOfTestBuffers> buff{};
+		time_point<high_resolution_clock> start_time = high_resolution_clock::now();
+		for (uint64_t iter = 0; iter < amountr_of_test_iterations; iter++) {
+			for (uint64_t i = 0; i < kSizeOfTestSet; i++) {
+				buff.push_back(test_data[i]);
+			}
+		}
+		uint64_t test_time_mk = std::chrono::duration_cast<std::chrono::microseconds>(high_resolution_clock::now() - start_time).count();
+		cout << " adsvel::CircularBuffer fill time: " << test_time_mk << "mks" << endl;
+	}
+    {
+        boost::circular_buffer<tTestType> buff{kSizeOfTestBuffers};
+        time_point<high_resolution_clock> start_time = high_resolution_clock::now();
+        for (uint64_t iter = 0; iter < amountr_of_test_iterations; iter++) {
+            for (uint64_t i = 0; i < kSizeOfTestSet; i++) {
+                buff.push_back(test_data[i]);
+            }
+        }
+        uint64_t test_time_mk = std::chrono::duration_cast<std::chrono::microseconds>(high_resolution_clock::now() - start_time).count();
+        cout << " boost::circular_buffer fill time: " << test_time_mk << "mks" << endl;
+    }
     cout << endl;
     return 0;
 }
